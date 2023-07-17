f232a06d838442b642e4f6c1f0048988a9d0ee64
elkana.widersen <elkana.widersen@seamoney.com>
2023-07-17 15:55:27 +0700

    feat: add postinstall script

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -42,6 +42,8 @@ export interface ButtonProps
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
   ({ className, variant, size, asChild = false, ...props }, ref) => {
     const Comp = asChild ? Slot : "button"
+    const Test = <div className='text-primary-foreground'></div>;
+
     return (
       <Comp
         className={cn(buttonVariants({ variant, size, className }))}
